7727f32ceb53273e6edfd462c4b21eca4cef5d70
kovarikd <dave.kovi@gmail.com>
2017-10-23 21:47:34 +0200

    Refactored of Verify_inpu() function

--- TicTacToe_Game.py
+++ TicTacToe_Game.py
@@ -1,3 +1,6 @@
+ttt_matrix = [[" ", "|", " ", "|", " "], ["---", "---", "--"], [" ", "|", " ", "|", " "], ["---", "---", "--"], [" ", "|", " ", "|", " "]]
+
+
 def print_matrix(matrix):
     for element in matrix:
         matrix_output = ""
@@ -6,75 +9,62 @@ def print_matrix(matrix):
         print(matrix_output)
 
 
-def verify_input(inpt, player, matrix):
+def verify_input(inpt, player):
     inpt = inpt.upper()
+    global ttt_matrix
     if inpt == "END":
         print("Game was terminated!")
         exit(12)
-    elif inpt == "11" and matrix[0][0] == " ":
-        if player == "Player_1":
-            matrix[0][0] = "O"
-        else:
-            matrix[0][0] = "X"
-        return matrix
-    elif inpt == "12" and matrix[0][2] == " ":
-        if player == "Player_1":
-            matrix[0][2] = "O"
-        else:
-            matrix[0][2] = "X"
-        return matrix
-    elif inpt == "13" and matrix[0][4] == " ":
-        if player == "Player_1":
-            matrix[0][4] = "O"
-        else:
-            matrix[0][4] = "X"
-        return matrix
-    elif inpt == "21" and matrix[2][0] == " ":
-        if player == "Player_1":
-            matrix[2][0] = "O"
-        else:
-            matrix[2][0] = "X"
-        return matrix
-    elif inpt == "22" and matrix[2][2] == " ":
-        if player == "Player_1":
-            matrix[2][2] = "O"
-        else:
-            matrix[2][2] = "X"
-        return matrix
-    elif inpt == "23" and matrix[2][4] == " ":
-        if player == "Player_1":
-            matrix[2][4] = "O"
-        else:
-            matrix[2][4] = "X"
-        return matrix
-    elif inpt == "31" and matrix[4][0] == " ":
-        if player == "Player_1":
-            matrix[4][0] = "O"
-        else:
-            matrix[4][0] = "X"
-        return matrix
-    elif inpt == "32" and matrix[4][2] == " ":
-        if player == "Player_1":
-            matrix[4][2] = "O"
-        else:
-            matrix[4][2] = "X"
-        return matrix
-    elif inpt == "33" and matrix[4][4] == " ":
-        if player == "Player_1":
-            matrix[4][4] = "O"
-        else:
-            matrix[4][4] = "X"
-        return matrix
+    elif inpt == "11" and ttt_matrix[0][0] == " " and player == "Player_1":
+        ttt_matrix[0][0] = "O"
+    elif inpt == "11" and ttt_matrix[0][0] == " " and player == "Player_2":
+        ttt_matrix[0][0] = "X"
+    elif inpt == "12" and ttt_matrix[0][2] == " " and player == "Player_1":
+        ttt_matrix[0][2] = "O"
+    elif inpt == "12" and ttt_matrix[0][2] == " " and player == "Player_2":
+        ttt_matrix[0][2] = "X"
+    elif inpt == "13" and ttt_matrix[0][4] == " " and player == "Player_1":
+        ttt_matrix[0][4] = "O"
+    elif inpt == "13" and ttt_matrix[0][4] == " " and player == "Player_2":
+        ttt_matrix[0][4] = "X"
+    elif inpt == "21" and ttt_matrix[2][0] == " " and player == "Player_1":
+        ttt_matrix[2][0] = "O"
+    elif inpt == "21" and ttt_matrix[2][0] == " " and player == "Player_2":
+        ttt_matrix[2][0] = "X"
+    elif inpt == "22" and ttt_matrix[2][2] == " " and player == "Player_1":
+        ttt_matrix[2][2] = "O"
+    elif inpt == "22" and ttt_matrix[2][2] == " " and player == "Player_2":
+        ttt_matrix[2][2] = "X"
+    elif inpt == "23" and ttt_matrix[2][4] == " " and player == "Player_1":
+        ttt_matrix[2][4] = "O"
+    elif inpt == "23" and ttt_matrix[2][4] == " " and player == "Player_2":
+        ttt_matrix[2][4] = "X"
+    elif inpt == "31" and ttt_matrix[4][0] == " " and player == "Player_1":
+        ttt_matrix[4][0] = "O"
+    elif inpt == "31" and ttt_matrix[4][0] == " " and player == "Player_2":
+        ttt_matrix[4][0] = "X"
+    elif inpt == "31" and ttt_matrix[4][0] == " " and player == "Player_1":
+        ttt_matrix[4][2] = "O"
+    elif inpt == "31" and ttt_matrix[4][0] == " " and player == "Player_2":
+        ttt_matrix[4][2] = "X"
+    elif inpt == "33" and ttt_matrix[4][4] == " " and player == "Player_1":
+        ttt_matrix[4][4] = "O"
+    elif inpt == "33" and ttt_matrix[4][4] == " " and player == "Player_2":
+        ttt_matrix[4][4] = "X"
     else:
         print("\n")
         print("OUTPUT:: {}:Invalid input, you wasted your turn.".format(player))
         print("\n")
-        return matrix
+        return ttt_matrix
+
+
+def game_resoution():
+    if ttt_matrix[0][0] and ttt_matrix[0][2] and ttt_matrix[0][4]:
 
 
 def main():
     help_matrix = [[11, "|", 12, "|", 13], ["---", "---", "--"], [21, "|", 22, "|", 23], ["---", "---", "--"], [31, "|", 32, "|", 33]]
-    ttt_matrix = [[" ", "|", " ", "|", " "], ["---", "---", "--"], [" ", "|", " ", "|", " "], ["---", "---", "--"], [" ", "|", " ", "|", " "]]
+    global ttt_matrix
     current_player = "Player_1"
     while True:
         print("Here are the coordinations in the Tic Tac Toe matrix")
@@ -87,11 +77,11 @@ def main():
         print_matrix(ttt_matrix)
         if current_player == "Player_1":
             pick = input("Player 1: Please pick your your field")
-            ttt_matrix = verify_input(pick, current_player, ttt_matrix)
+            verify_input(pick, current_player)
             current_player = "Player_2"
         else:
             pick = input("Player 1: Please pick your your field")
-            ttt_matrix = verify_input(pick, current_player, ttt_matrix)
+            verify_input(pick, current_player)
             current_player = "Player_1"
 
 
